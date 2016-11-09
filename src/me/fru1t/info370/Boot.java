1f8e7233f0465d2b657abc29b5bb22261a86a2e5
Kodlee Yin <kodlee@fru1t.me>
2016-11-09 09:45:45 -0800

    Abstracted and coalesced processes into a package outside of Boot

--- src/me/fru1t/info370/Boot.java
+++ src/me/fru1t/info370/Boot.java
@@ -1,72 +1,8 @@
 package me.fru1t.info370;
 
-import java.io.FileNotFoundException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Types;
-
-import me.fru1t.info370.tables.Post;
-
 public class Boot {
-	public static void main(String[] args) throws FileNotFoundException, SQLException, InterruptedException {
-		XmlRowReader<Post> reader = new XmlRowReader<Post>("D:\\stack\\stackoverflow\\stackoverflow.com-Posts\\Posts.xml", Post.class);
-		long rows = 0;
-		Connection connection = DriverManager.getConnection(Database.CONNECTION_STRING);
-		PreparedStatement stmt = connection.prepareStatement(Post.MYSQL_INSERT);
-		
-		while (!reader.isComplete()) {
-			Post p = reader.next();
-			if (p != null) {
-				try {
-					stmt.setInt(Post.COL_ID, p.getId());
-					stmt.setInt(Post.COL_POST_TYPE_ID, p.getPostTypeId());
-				} catch (NumberFormatException e) {
-					System.out.println("Skipped row " + p.toString());
-					continue;
-				}
-				
-				int acceptedAnswerId = p.getAcceptedAnswerId();
-				if (acceptedAnswerId == -1) {
-					stmt.setNull(Post.COL_ACCEPTED_ANSWER_ID, Types.INTEGER);
-				} else {
-					stmt.setInt(Post.COL_ACCEPTED_ANSWER_ID, acceptedAnswerId);
-				}
-				
-				int parentId = p.getParentId();
-				if (parentId == -1) {
-					stmt.setNull(Post.COL_PARENT_ID, Types.INTEGER);
-				} else {
-					stmt.setInt(Post.COL_PARENT_ID, parentId);
-				}
-	
-				stmt.setInt(Post.COL_OWNER_USER_ID, p.getOwnerUserId());
-				stmt.setInt(Post.COL_CREATION_DATE, p.getCreationDate());
-				stmt.setInt(Post.COL_LAST_EDIT_DATE, p.getLastEditDate());
-				stmt.setInt(Post.COL_LAST_ACTIVITY_DATE, p.getLastActivityDate());
-				stmt.setInt(Post.COL_VIEW_COUNT, p.getViewCount());
-				stmt.setInt(Post.COL_ANSWER_COUNT, p.getAnswerCount());
-				stmt.setInt(Post.COL_COMMENT_COUNT, p.getCommentCount());
-				stmt.setInt(Post.COL_FAVORITE_COUNT, p.getFavoriteCount());
-				stmt.setInt(Post.COL_SCORE, p.getScore());
-				stmt.setString(Post.COL_TAGS, p.getTags());
-				stmt.setString(Post.COL_TITLE, p.getTitle());
-				
-				stmt.addBatch();
-				
-				if (rows++ % 20000 == 0) {
-					System.out.println("Committing row " + p.getId() + " (" + rows + " rows)");
-					try {
-						stmt.executeBatch();
-					} catch (SQLException e) {
-						System.out.println("Failed to mysql on " + e.getMessage());
-					}
-				}
-			}
-		}
-		
-		stmt.executeBatch();
+	public static void main(String[] args) {
+
 		
 	}
 }
---
+++ src/me/fru1t/info370/processes/UserProcess.java
@@ -0,0 +1,56 @@
+package me.fru1t.info370.processes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import me.fru1t.info370.Database;
+import me.fru1t.info370.XmlRowReader;
+import me.fru1t.info370.tables.User;
+
+public class UserProcess implements Runnable {
+
+	@Override
+	public void run() {
+		try {
+			Connection connection = DriverManager.getConnection(Database.CONNECTION_STRING);
+			PreparedStatement stmt = connection.prepareStatement(User.MYSQL_INSERT);
+			long rows = 0;
+			
+			XmlRowReader<User> reader = new XmlRowReader<User>("D:\\stack\\stackoverflow\\stackoverflow.com-Users\\Users.xml", User.class);
+			while (!reader.isComplete()) {
+				User u = reader.next();
+				if (u != null) {
+					
+					try {
+						stmt.setInt(User.COLUMN_ID, u.getId());
+					} catch (NumberFormatException e) {
+						e.printStackTrace();
+						continue;
+					}
+					
+					stmt.setInt(User.COLUMN_REPUTATION, u.getReputation());
+					stmt.setInt(User.COLUMN_CREATION_DATE, u.getCreationDate());
+					stmt.setString(User.COLUMN_DISPLAY_NAME, u.getDisplayName());
+					stmt.setString(User.COLUMN_LOCATION, u.getLocation());
+					stmt.setInt(User.COLUMN_VIEWS, u.getViews());
+					stmt.setInt(User.COLUMN_UPVOTES, u.getUpVotes());
+					stmt.setInt(User.COLUMN_DOWNVOTES, u.getDownVotes());
+					stmt.setInt(User.COLUMN_AGE, u.getAge());
+					stmt.addBatch();
+					
+					if (rows++ % 20000 == 0) {
+						System.out.println("Commiting row " + rows);
+						stmt.executeBatch();
+					}
+				}
+			}
+			
+			System.out.println("Final commit at row " + rows);
+			stmt.executeBatch();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
